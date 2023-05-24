c49da23252ba9640413a1264b98f219b10ca6d78
K <krseven@gmail.com>
2023-05-23 23:07:55 -0500

    add Upcoming ViewController and cells

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by J on 5/23/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
